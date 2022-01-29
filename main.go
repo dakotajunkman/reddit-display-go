4fe29619a2a40be350bd55852ae29dfafbe047cb
Dakota Junkman <dakota.junkman@gmail.com>
2022-01-29 11:17:31 -0600

    basic http functionality

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"log"
+	"io/ioutil"
 )
 
 func main() {
 	fmt.Println("This works")
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
 }

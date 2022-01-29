96cdb3c7e0985bd37ad7103d09b6e2cbc1ff0e3e
Dakota Junkman <dakota.junkman@gmail.com>
2022-01-29 16:34:55 -0600

    complete up to http call

--- main.go
+++ main.go
@@ -2,22 +2,69 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"log"
-	"io/ioutil"
+	// "io/ioutil"
+	// "log"
+	// "net/http"
+	"strings"
 )
 
 func main() {
-	fmt.Println("This works")
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
-	if err != nil {
-		log.Fatal(err)
+	printIntro()
+	usernamePrompt()
+	username := getInput()
+
+	if len(username) == 0 {
+		meanKeywordPrompt()
+	} else {
+		skippableKeywordPrompt()
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
+	keyword := getInput()
+	
+	if len(keyword) == 0 && len(username) == 0 {
+		if tryAgain(){
+			main()
+			return  // stops execution from continuing after recursion
+		}
+		return
 	}
+}
 
-	fmt.Println(string(body))
+func printIntro() {
+	fmt.Println("This neat program allows you to filter posts to the r/OSUOnlineCS subreddit by " +
+	"username or keyword, just like you've always wanted. You must enter at least one search " +
+	"parameter, but can do both if you want. If you enter none you will get in trouble and have " +
+	"to do it again. To skip a parameter, simply hit Enter.")
+}
+
+func usernamePrompt() {
+	fmt.Print("Username (Press Enter to submit): ")
+}
+
+func getInput() string {
+	var input string
+	fmt.Scanln(&input)
+
+	// trim off any whitespace
+	return strings.TrimSpace(input)
+}
+
+func skippableKeywordPrompt() {
+	fmt.Print("Keyword (can be skipped) (Press Enter to submit): ")
+}
+
+func meanKeywordPrompt() {
+	fmt.Print("Keyword (skip at your own risk) " +
+	"(Press Enter to submit): ")
+}
+
+func tryAgain() bool {
+	fmt.Println("Not cool. I specifically told you not to " +
+	"leave both fields blank.")
+
+	var again string
+	fmt.Print("Do you want to try again? y/n ")
+	fmt.Scanln(&again)
+
+	return strings.ToLower(again) == "y"
 }
